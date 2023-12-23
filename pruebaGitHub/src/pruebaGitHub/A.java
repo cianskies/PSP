38271e4cae1973596e3d98508e3ab66fe20d0831
cianskies <119041806+cianskies@users.noreply.github.com>
2023-12-23 12:10:17 +0100

    prueba 3 portatil

--- pruebaGitHub/src/pruebaGitHub/A.java
+++ pruebaGitHub/src/pruebaGitHub/A.java
@@ -4,5 +4,6 @@ public class A{
 	public static void main(String[] args) {
 		System.out.println("Hola.");
 		System.out.println("Hola desde el portatil");
+		System.out.println("Portatil prueba 3");
 	}
 }
